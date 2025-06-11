f4a96384bf86705347ba3d5f059ca7dee6d3d7c5
juliaszczepanek <juliszczepanek12@gmail.com>
2025-06-11 15:35:18 +0200

    Add superuser creation script

--- srebrna_backend/create_admin.py
+++ srebrna_backend/create_admin.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-def run():
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin", email="admin@example.com", password="Sup3rSecret2025@"
-        )
+username = "admin"
+password = "Sup3rSecret2025@"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("✅ Superuser created.")
+else:
+    print("ℹ️ Superuser already exists.")

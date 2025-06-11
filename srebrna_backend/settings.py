3e6c9097ba83dea517845251c5c94724a1d081ea
juliaszczepanek <juliszczepanek12@gmail.com>
2025-06-11 12:18:04 +0200

    use .env for secret key

--- srebrna_backend/settings.py
+++ srebrna_backend/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-53_ws_sonqd2zv=tonz-uq2ybvjl@4ase0fug!mdybdj$rwba("
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-unsafe-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

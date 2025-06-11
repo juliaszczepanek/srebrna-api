7b05d453705e3adb85ee2ec7fbc1e6328b57864a
juliaszczepanek <juliszczepanek12@gmail.com>
2025-06-11 13:42:20 +0200

    Enable media file serving

--- srebrna_backend/urls.py
+++ srebrna_backend/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from articles.views import ArticleViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register(r"articles", ArticleViewSet)
@@ -10,3 +13,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

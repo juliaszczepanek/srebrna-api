3ad552b6cbda0ddc6549e0854a0703ed92d1c9e0
juliaszczepanek <juliszczepanek12@gmail.com>
2025-06-11 14:20:33 +0200

    update database settings

--- srebrna_backend/settings.py
+++ srebrna_backend/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = "srebrna_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db.cbiaakcktuvbdpnvysjt.supabase.co",
+        "PORT": "5432",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": os.getenv("SUPABASE_DB_PASSWORD"),
     }
 }
 

51d17cc1e8b080f7bd821af9d6edb05b119cd557
juliaszczepanek <juliszczepanek12@gmail.com>
2025-06-11 12:29:42 +0200

    Add Render domain to ALLOWED_HOSTS

--- srebrna_backend/settings.py
+++ srebrna_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-unsafe-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["srebrna-api.onrender.com"]
 
 
 # Application definition
